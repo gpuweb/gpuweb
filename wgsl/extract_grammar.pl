d77d46706078ba13464bc58ef2c2a8c5686b7f73
David Neto <dneto@google.com>
2020-09-11 13:28:50 -0400

    Refactor into functions

--- wgsl/extract_grammar.pl
+++ wgsl/extract_grammar.pl
@@ -11,64 +11,79 @@ use Data::Dumper;
 my $show_raw = 1;
 my $show_dump = 1;
 
-my @grammar_lines = ();
-my $in_grammar = 0;
-while(<>) {
-  chomp;
-  my $line = $_;
-  if ($line =~ m/^<pre\s+class='def'/) {
-    $in_grammar = 1;
-  } elsif ($line =~ m/^<\/pre>/) {
-    $in_grammar = 0;
-  } elsif ($in_grammar) {
-    #    $line =~ s/--.*//; # For comments like "-- Capability"
-    $line =~ s/\s+/ /g;
-    push (@grammar_lines, $line) if is_valid($line);
+my @grammar_lines = GetGrammarLines(<>);
+my $raw_grammar = Parse(@grammar_lines);
+print Dumper($raw_grammar) if $show_dump;
+exit 0;
+
+# Returns the list of grammar-defining lines from the WGSL spec source.
+# Input is the list of lines from the WGSL spec source.
+sub GetGrammarLines(@) {
+  my (@input) = @_;
+  my $in_grammar = 0;
+  my @grammar_lines = ();
+  foreach (@input) {
+    chomp;
+    my $line = $_;
+    if ($line =~ m/^<pre\s+class='def'/) {
+      $in_grammar = 1;
+    } elsif ($line =~ m/^<\/pre>/) {
+      $in_grammar = 0;
+    } elsif ($in_grammar) {
+      #    $line =~ s/--.*//; # For comments like "-- Capability"
+      $line =~ s/\s+/ /g;
+      push (@grammar_lines, $line) if is_valid($line);
+    }
   }
+  return @grammar_lines;
 }
 
-# Key is a terminal or nonterminal in the grammar.
-# The value is insignificant.
-my %symbol = ();
-# Key is the production result, value is right-hand-sides
-# Each right-hand-side is a list of tokens.
-my %rule = ();
+# Transforms a list of grammar lines into a structured representation
+# of the grammar.
+sub Parse(@) {
+  my (@grammar_lines) = @_;
+  # Key is a terminal or nonterminal in the grammar.
+  # The value is insignificant.
+  my %symbol = ();
+  # Key is the production result, value is right-hand-sides
+  # Each right-hand-side is a list of tokens.
+  my %rule = ();
 
-# Name of the nonterminal on the left-hand-side of rules
-# that we are currently parsing.
-my $lhs = undef;
-# A reference to the most recently addeded rule right-hand-side.
-my $last_rhs = undef;
-foreach my $line (@grammar_lines) {
-  print $line,"\n" if $show_raw;
-  if ($line =~ m/^(\S+)$/) {
-    my $name = $1;
-    die "more than one definition for $name" if exists $rule{$name};
-    $lhs = $name;
-    $symbol{$lhs} = 1;
-    $rule{$name} = [];
-  } elsif ($line =~ m/^ [:|]/) {
-    my @parts = split(/\s+/, $line);
-    shift @parts; # Skip the initial ' '
-    shift @parts; # Skip the initial : or |
-    @parts = split_re(@parts);
-    foreach my $part (@parts) { $symbol{$part} = 1; }
-    $last_rhs = [@parts];
-    push @{$rule{$lhs}}, $last_rhs;
-  } elsif ($line =~ m/^ [\(|\w]/) {
-    # Assume this is a continuation of the most recent RHS
-    my @parts = split(/\s+/, $line);
-    shift @parts; # Skip the initial ' '
-    @parts = split_re(@parts);
-    die unless defined $last_rhs;
-    push @$last_rhs, @parts;
-  } else {
-    die "unknown line: $line";
+  # Name of the nonterminal on the left-hand-side of rules
+  # that we are currently parsing.
+  my $lhs = undef;
+  # A reference to the most recently addeded rule right-hand-side.
+  my $last_rhs = undef;
+  foreach my $line (@grammar_lines) {
+    print $line,"\n" if $show_raw;
+    if ($line =~ m/^(\S+)$/) {
+      my $name = $1;
+      die "more than one definition for $name" if exists $rule{$name};
+      $lhs = $name;
+      $symbol{$lhs} = 1;
+      $rule{$name} = [];
+    } elsif ($line =~ m/^ [:|]/) {
+      my @parts = split(/\s+/, $line);
+      shift @parts; # Skip the initial ' '
+      shift @parts; # Skip the initial : or |
+      @parts = split_re(@parts);
+      foreach my $part (@parts) { $symbol{$part} = 1; }
+      $last_rhs = [@parts];
+      push @{$rule{$lhs}}, $last_rhs;
+    } elsif ($line =~ m/^ [\(|\w]/) {
+      # Assume this is a continuation of the most recent RHS
+      my @parts = split(/\s+/, $line);
+      shift @parts; # Skip the initial ' '
+      @parts = split_re(@parts);
+      die unless defined $last_rhs;
+      push @$last_rhs, @parts;
+    } else {
+      die "unknown line: $line";
+    }
   }
+  my @symbols = sort keys %symbol;
+  return [symbol => [@symbols] , rule => \%rule];
 }
-my @symbols = sort keys %symbol;
-
-print Dumper([symbol => [@symbols] , rule => \%rule]) if $show_dump;
 
 # Returns 0 if the line is invalid, and 1 otherwise.
 sub is_valid {
@@ -83,7 +98,8 @@ sub is_valid {
   return 0 if $line eq '';
   return 1;
 }
-# Tranfsorms a list of strings to break up regular expression
+
+# Transforms a list of strings to break up regular expression
 # meta-symbols into their own individual tokens. For example,
 # an input like:
 #    '(ab', '|', 'c)?'
