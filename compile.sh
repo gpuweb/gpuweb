3cec809f111a4814b7f06cf59038a072540d9b42
Kai Ninomiya <kainino@chromium.org>
2021-04-01 16:49:29 -0700

    redirect wgsl.html to wgsl/

--- compile.sh
+++ compile.sh
@@ -10,10 +10,14 @@ make -C explainer
 
 if [ -d out ]; then
   mkdir out/wgsl out/explainer
+
   echo 'Copying spec/index.html spec/webgpu.idl -> out/'
   cp spec/index.html spec/webgpu.idl out/
+
   echo 'Copying wgsl/index.html -> out/wgsl/'
   cp wgsl/index.html out/wgsl/
+  echo '<meta http-equiv="refresh" content="0;url=wgsl/" />' > out/wgsl.html
+
   echo 'Copying explainer/index.html -> out/explainer/'
   cp explainer/index.html out/explainer/
 fi
