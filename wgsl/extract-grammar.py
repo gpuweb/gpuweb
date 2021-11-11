bd7822d1699aad173a6ffae4bb474236f68f10c7
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2021-11-11 17:47:59 +0300

    Remove reset method and exemplify usage

--- wgsl/extract-grammar.py
+++ wgsl/extract-grammar.py
@@ -440,7 +440,6 @@ enum TokenType {
 
 void *tree_sitter_wgsl_external_scanner_create() { return NULL; }
 void tree_sitter_wgsl_external_scanner_destroy(void *p) {}
-void tree_sitter_wgsl_external_scanner_reset(void *p) {}
 unsigned tree_sitter_wgsl_external_scanner_serialize(void *p, char *buffer) { return 0; }
 void tree_sitter_wgsl_external_scanner_deserialize(void *p, const char *b, unsigned n) {}
 
