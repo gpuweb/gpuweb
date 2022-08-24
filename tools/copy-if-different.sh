354f91c692a3fad0ff9a23a2d6bea40e04a6f9c2
David Neto <dneto@google.com>
2022-08-24 10:24:11 -0400

    publish grammar without left-recursion; add tooling to make it so (#3292)
    * Add non-normative section with left recursion removed
    * Add tooling to rewrite the grammar so it recognizes the same strings, but has left recursion removed.
    * Add recursive and recursive_ll Makefile targets
    * Add copy-if-different.sh Bourne shell
    
    Tooling highlights:
    * Fix canonicalization so it no longer drops some rules (!)
    * Add Grammar.preorder()
    * Add Grammar.eliminate_left_recursion
    * Add Grammar.hoist_until
    * Sort output
    * Left-refactor until unary_expression bubbles up to the top
    * Inline trivial cases having epsilons
    * Add inline_single_choice_with_nonterminal
    * Generate FOO ?  and FOO * when emitting
    * Skip printing token rules by default
    * Heuristically reabsorb unary_expression, simplifying relational_expression
    * Left-factor ident
    * Hoist trivial X | Y
    * Don't absorb ident too aggressively
    * Don't inline member_ident or ident_pattern_token
    * Rotate things like  (a b)* a b
    * Generalize rotation
    * Rewrite  (a b c)* a b c ->  a b c (a b c)*
    * Rewrite X when both X and X.post.unary_expression exist
    * Add LLReduce, so we can print LL(1) tables again
    * Allow LL(1) analysis of the result of left-recursion
    * When left-refactoring, rewrite the refactored rule
    * Don't delete the start symbol
    * Add option for -bs bikeshed output
    
    Fixes: #3286

---
+++ tools/copy-if-different.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+usage()
+{
+    echo "Usage: $0 input_file output_file"
+    echo
+    echo "Copies input_file to output_file if output_file does not exist or"
+    echo "is different from input_file"
+    exit 1
+}
+
+[ $# -eq 2 ] || usage
+input="$1"
+output="$2"
+if [ ! -f "$input" ]; then
+  echo "input file does not exist: $input"
+  usage
+fi
+
+if [ ! -f "$output" ] || ! diff "$input" "$output" >/dev/null; then
+  cp "$input" "$output"
+fi
+
