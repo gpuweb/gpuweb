b24508b4e53d82cea2fe9978e6b2403d5ae377bb
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2023-07-06 02:03:31 +0900

    Gather files

--- tools/custom-action/entrypoint.sh
+++ tools/custom-action/entrypoint.sh
@@ -3,7 +3,7 @@ set -eo pipefail
 
 cp -r /grammar ./wgsl/
 export PATH="$(python3 -m site --user-base)/bin:${PATH}"
-git init
+git init # To ensure subsequent git commands pick the workspace
 git config --global --add safe.directory /github/workspace
 BIKESHED_DISALLOW_ONLINE=1 REQUIRE_DIAGRAM_GENERATION=1 make -j out
 bash tools/check-repo-clean.sh
