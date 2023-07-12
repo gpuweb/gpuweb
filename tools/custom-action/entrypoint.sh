e1827d9f5f88c61d923c5a790965583371723a60
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2023-07-13 04:59:56 +0900

    Source in entrypoint

--- tools/custom-action/entrypoint.sh
+++ tools/custom-action/entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash --login
 set -eo pipefail
 
-bash /prepare.sh # Execute the prepare script
+. /prepare.sh # Execute the prepare script
 git init # To ensure subsequent git commands pick the workspace
 git config --global --add safe.directory /github/workspace
 BIKESHED_DISALLOW_ONLINE=1 REQUIRE_DIAGRAM_GENERATION=1 make -j out
