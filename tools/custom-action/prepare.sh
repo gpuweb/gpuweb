65529d23ed7a911ec949baa4bfd5dbdec2d0dcf2
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2023-12-07 03:12:55 +0900

    Hotfix1: Even more explicitly run install Puppeteer (#4410)
    * Update action.yml
    
    * Update prepare.sh
    
    * Update action.yml

--- tools/custom-action/prepare.sh
+++ tools/custom-action/prepare.sh
@@ -7,3 +7,4 @@ python3 -m pip install --break-system-packages --upgrade \
   bikeshed
 export PATH="$(python3 -m site --user-base)/bin:${PATH}"
 bikeshed update
+node "/usr/local/lib/node_modules/@mermaid-js/mermaid-cli/node_modules/puppeteer/install.js"
