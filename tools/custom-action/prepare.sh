f0652ed3a738cec4f23084be30722deeed776726
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2025-07-01 12:14:03 +0000

    CI maintenance to avoid CVEs
    This PR upgrades tool versions and changes the base image for CI & devcontainer to Ubuntu:25.04 because both Bookworm and Trixie (upcoming release) images have CVEs (for both normal and slim images). When doing this, one important consideration was Chromium as needed by Puppeteer (which Mermaid uses to drive diagram generation); however, installing Puppeteer separately earlier and running the install script with its updated path resolves the issue (and it turns out Puppeteer was installing its own version for the browser in any case). Also, to improve workflow across multiple image builds, this PR adds a .dockerignore file to not copy node_modules from the host to the container. Also, the post-create script is adjusted to avoid the Bikeshed install issue by ignoring installed, which appears from time to time during the devcontainer creation process.
    
    Thank you very much in advance!
    
    PS: I tried to avoid .dockerignore at the top; however, the `
    `--exclude` argument to `COPY` or placing the file next to Dockerfile itself under the tools directory didn't work uniformly across the devices I use to test the version upgrades. The TS CLI version upgrade will be done after ABI adjustment; the current one has a different import approach than the latest.

--- tools/custom-action/prepare.sh
+++ tools/custom-action/prepare.sh
@@ -2,9 +2,14 @@
 set -euo pipefail
 
 source /dependency-versions.sh # Source dependency versions
-cp -r /grammar ./wgsl/
-python3 -m pip install --break-system-packages --upgrade \
+if [ -d "./wgsl/grammar" ]; then
+  rm -rf "./wgsl/grammar"
+fi
+if [ -d "/grammar" ]; then
+  cp -r "/grammar" "./wgsl/"
+fi
+python3 -m pip install --break-system-packages  --ignore-installed --upgrade \
   bikeshed==$PIP_BIKESHED_VERSION
 export PATH="$(python3 -m site --user-base)/bin:${PATH}"
 bikeshed update
-node "/usr/local/lib/node_modules/@mermaid-js/mermaid-cli/node_modules/puppeteer/install.js"
+node "/usr/local/lib/node_modules/puppeteer/install.mjs"
