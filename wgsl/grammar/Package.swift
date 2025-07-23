d525280706261802a18c17a2efcfdbf1d8986b3d
Mehmet Oguz Derin <mehmetoguzderin@mehmetoguzderin.com>
2025-07-23 21:14:12 +0000

    Fix

--- wgsl/grammar/Package.swift
+++ wgsl/grammar/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
         .library(name: "TreeSitterWgsl", targets: ["TreeSitterWgsl"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0"),
+        .package(url: "https://github.com/tree-sitter/swift-tree-sitter", from: "0.8.0"),
     ],
     targets: [
         .target(
